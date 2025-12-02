d66f63f799caa50816c1e8e923dab0c05076d8a1
adm <kaio.candf@gmail.com>
2025-12-02 16:24:41 -0300

    criado deletar usuario

--- src/main/java/com/example/Produtos/API/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/example/Produtos/API/produtosapi/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import com.example.Produtos.API.produtosapi.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +46,8 @@ public class ProdutoController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable String id){
+        produtoRepository.deleteById(id);
+    }
 }
