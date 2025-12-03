ad4486ea855f432db96776b8abe041977af267ba
adm <kaio.candf@gmail.com>
2025-12-03 10:46:32 -0300

    adicionado função para buscar produtos por nome

--- src/main/java/com/example/Produtos/API/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/com/example/Produtos/API/produtosapi/repository/ProdutoRepository.java
@@ -3,5 +3,8 @@ package com.example.Produtos.API.produtosapi.repository;
 import com.example.Produtos.API.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+    List<Produto> findByName(String name);
 }
