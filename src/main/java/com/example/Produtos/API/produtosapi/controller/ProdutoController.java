3532fee50c1e4371be96b0783ad09da7cd7e212b
adm <kaio.candf@gmail.com>
2025-12-03 10:29:35 -0300

    atualizando dados com put

--- src/main/java/com/example/Produtos/API/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/example/Produtos/API/produtosapi/controller/ProdutoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,4 +51,11 @@ public class ProdutoController {
     public void deletar(@PathVariable String id){
         produtoRepository.deleteById(id);
     }
+
+    @PutMapping("/{id}")
+    public void atualizarProdutoId(@PathVariable String id, @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
 }
